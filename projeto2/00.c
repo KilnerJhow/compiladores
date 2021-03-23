3c63e101e0d585d526f5ed713a951f260565ffcb
Jonathan Kilner <jonathankilner@hotmail.com>
2021-03-23 12:26:29 -0300

    autogen

--- projeto2/00.c
+++ projeto2/00.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+int unum = 1;
+
+float half (float x) {
+	return x / 2;
+}
+
+int scalar (int a, int b) {
+	int array[10];
+	for (int i = 0; i < 10; i++) {
+		if (a < b) a -= b;
+		array[i] = a;
+	}
+	/***********
+	 * comment *
+	 ***********/
+	if (a > b) {
+		a /= b - unum * 2.5 - (4 <= a - b);
+	} else if (a < b) {
+		b += a / -unum + (8 >= b * a);
+	} else {
+		b = a;
+	}
+
+	return half(a * a + b + (b - a) * b);
+}
 
 void main(){
-    int a = 1.999999 + b;
-    printf("%d\n", a);
+    int kkkk[3] = {1, "12", 1.0};
 }
